f6f464f70933f2f4ac26c752ba6a3c49ba4e9ead
Oleg <oleg13fedorov@yandex.ru>
2022-04-14 22:42:49 +0300

    con_database

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import pandas as pd
+import psycopg2
+connection = psycopg2.connect(database="students",
+ user="postgres",
+ password="Qazwsxedcf1-",
+ host="127.0.0.1",
+ port="5432")
+
+cursor = connection.cursor()
+print(connection.get_dsn_parameters(), "\n")
 print(5)
 print(4)
